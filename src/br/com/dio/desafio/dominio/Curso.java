228be4dcabb6126f944c3698a519693423c701eb
Kauê Felippe <111477957+kauefelippet@users.noreply.github.com>
2024-10-05 14:36:06 -0300

    added Bootcamp classes

---
+++ src/br/com/dio/desafio/dominio/Curso.java
@@ -0,0 +1,28 @@
+package br.com.dio.desafio.dominio;
+
+public class Curso extends Conteudo {
+
+    private int cargaHoraria;
+
+    public int getCargaHoraria() {
+        return cargaHoraria;
+    }
+
+    public void setCargaHoraria(int cargaHoraria) {
+        this.cargaHoraria = cargaHoraria;
+    }
+
+    @Override
+    public String toString() {
+        return "Curso{" +
+                "cargaHoraria=" + cargaHoraria +
+                ", nome='" + getTitulo() + '\'' +
+                ", descricao='" + getDescricao() + '\'' +
+                '}';
+    }
+
+    @Override
+    public double calcularXp() {
+        return XP_PADRAO * cargaHoraria;
+    }
+}
